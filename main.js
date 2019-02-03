79195c820432b9caca3f9ceffb89f980e7c7c94a
Saveleva <saveleva_ol@mail.ru>
2019-02-04 00:17:49 +0300

    Work-1-2

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
-function draw(el){
-  el.style.border ='3px solid #FFFF00';
+// -------------------------Work-1-----------------------------
+// function draw(el){
+//   el.style.border ='3px solid #FFFF00';
+// }
+//
+// function drawTwo(el){
+//   el.style.border='5px solid green';
+// }
+//
+// var t=document.getElementById('Deep');
+// function getParent(el){
+//   if ( !el || typeof el !== "object" ){ alert("Nope"); return}
+//   draw(el);
+// 	if ( el.id === "Start" ){return}
+//   if ( el.parentElement ){console.log(1);
+// 	document.getElementById("Deep").innerText += ' '+el.tagName;
+//       setTimeout( ()=>{ getParent(el.parentElement) }, 300 );
+//   }
+// }
+// getParent(t);
+
+
+// --------------------------Work-2-----------------------------------
+
+function red(el){
+  el.style.border='3px solid red';
+}
+function green(el){
+  el.style.border='3px solid #00FF00';
 }
 
-function drawTwo(el){
-  el.style.border='5px solid green';
-}
+t=document.getElementById('Child1');
+k=document.getElementById('Child2');
+function getChild(el){
 
-var t=document.getElementById('Deep');
-function getParent(el){
-  if ( !el || typeof el !== "object" ){ alert("Nope"); return}
-  draw(el);
-	if ( el.id === "Start" ){return}
-  if ( el.parentElement ){console.log(1);
-	document.getElementById("Deep").innerText += ' '+el.tagName;
-      setTimeout( ()=>{ getParent(el.parentElement) }, 300 );
-  }
+    if(el.id!=="Sib1"){
+      red(el);
+      setTimeout(()=>{getChild(el.parentElement)}, 300);}
+    else {
+      el=el.nextElementSibling;
+      deep(el);
+    }
+
 }
-getParent(t);
+getChild(t);
+
+function deep(el){
+  if (el.id!=="Sib2"){ green(el);}
+  const coll = Array.from(el.childNodes).filter(c=>c.tagName==="DIV");
+  coll.forEach( (c)=>{ setTimeout(()=>{deep(c)},300) } )
+}
