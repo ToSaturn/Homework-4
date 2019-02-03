acc6e5f1ec66a9e8319d7477f566628d89e01367
Saveleva <saveleva_ol@mail.ru>
2019-02-03 21:43:18 +0300

    Work-1

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-document.body.style.backgroundColor = 'red';
-alert( 'Поменяли цвет BODY' );
+function draw(el){
+  el.style.border ='3px solid #FFFF00';
+}
 
-document.body.style.backgroundColor = '';
-alert( 'Сбросили цвет BODY' );
+function drawTwo(el){
+  el.style.border='5px solid green';
+}
+
+var t=document.getElementById('Deep');
+function getParent(el){
+  if ( !el || typeof el !== "object" ){ alert("Nope"); return}
+  draw(el);
+	if ( el.id === "Start" ){return}
+  if ( el.parentElement ){console.log(1);
+	document.getElementById("Deep").innerText += ' '+el.tagName;
+      setTimeout( ()=>{ getParent(el.parentElement) }, 300 );
+  }
+}
+getParent(t);
